be80f28d759f8ff707a2e9a8859604d089b19aa1
Jackson Greene <19760458@student.curtin.edu.au>
2020-01-26 01:03:03 +0800

    neater

--- scrapper.py
+++ scrapper.py
@@ -14,7 +14,7 @@ soup = BeautifulSoup(driver.page_source, features="lxml")
 
 
 #/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-shelf-renderer/div[1]/div[2]/ytd-expanded-shelf-contents-renderer/div
-
+#this doesn't work
 video_list = soup.find("html").find("body").find("ytd-app").find("div").find("ytd-page-manager").find("ytd-browse").find("ytd-two-column-browse-results-renderer").findAll("div")[1].find("ytd-section-list-renderer").findAll("div")[2].findAll("ytd-item-section-renderer")[1].findAll("div")[3].find("ytd-shelf-renderer").find("div")[1].find("div")[2].find("ytd-expanded-shelf-contents-renderer")
 
 
@@ -27,9 +27,8 @@ print(video_list.prettify())
 response = requests.get(url)
 soup = BeautifulSoup(response.text, features="lxml")
 
-#/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]
 
-
+#this works but I don't know how
 for content in soup.findAll("div", class_= "yt-lockup-content"):
     try:
         title = content.h3.a.text
@@ -40,7 +39,4 @@ for content in soup.findAll("div", class_= "yt-lockup-content"):
 
     except Exception as e:
         description = None
- """
-#print(soup.find("html").find("body").findAll("ytd-app"))
-
-#print(soup.find("html").find("body").find("ytd-app"))#.find("div").find("ytd-page-manager").find("ytd-browse").find("ytd-two-column-browse-results-renderer").findAll("div")[1].findAll("ytd-section-list-renderer").findAll("div")[2].findAll("ytd-item-section-renderer")[1])
+ ##############
