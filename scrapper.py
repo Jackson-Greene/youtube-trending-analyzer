ff36d6684fd8ed445ed6f968afdd610bbaf053d7
Jackson Greene <19760458@student.curtin.edu.au>
2020-01-26 01:16:07 +0800

    generated html

--- scrapper.py
+++ scrapper.py
@@ -15,21 +15,26 @@ soup = BeautifulSoup(driver.page_source, features="lxml")
 
 #/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-shelf-renderer/div[1]/div[2]/ytd-expanded-shelf-contents-renderer/div
 #this doesn't work
-video_list = soup.find("html").find("body").find("ytd-app").find("div").find("ytd-page-manager").find("ytd-browse").find("ytd-two-column-browse-results-renderer").findAll("div")[1].find("ytd-section-list-renderer").findAll("div")[2].findAll("ytd-item-section-renderer")[1].findAll("div")[3].find("ytd-shelf-renderer").find("div")[1].find("div")[2].find("ytd-expanded-shelf-contents-renderer")
+#video_list = soup.find("html").find("body").find("ytd-app").find("div").find("ytd-page-manager").find("ytd-browse").find("ytd-two-column-browse-results-renderer").findAll("div")[1].find("ytd-section-list-renderer").findAll("div")[2].findAll("ytd-item-section-renderer")[1].findAll("div")[3].find("ytd-shelf-renderer").find("div")[1].find("div")[2].find("ytd-expanded-shelf-contents-renderer")
+
+
+with open("test.html", "w") as f:
+    f.write(driver.page_source)
+
 
 
 driver.quit()
 
-print(video_list.prettify())
+#print(video_list.prettify())
 
 
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, features="lxml")
+#response = requests.get(url)
+#soup = BeautifulSoup(response.text, features="lxml")
 
 
 #this works but I don't know how
-for content in soup.findAll("div", class_= "yt-lockup-content"):
+""" for content in soup.findAll("div", class_= "yt-lockup-content"):
     try:
         title = content.h3.a.text
         print(title)
@@ -38,5 +43,5 @@ for content in soup.findAll("div", class_= "yt-lockup-content"):
         print(description)
 
     except Exception as e:
-        description = None
+        description = None """
  ##############
