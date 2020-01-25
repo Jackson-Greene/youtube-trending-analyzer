c2cd62d7aaec2423473e7cf3c85472e001db4598
Jackson Greene <19760458@student.curtin.edu.au>
2020-01-26 00:48:59 +0800

    using selenium

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
 
 url = "https://www.youtube.com/feed/trending"
+
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+driver = webdriver.Chrome("./chromedriver", chrome_options=options)
+driver.get("https://www.youtube.com/feed/trending")
+
+soup = BeautifulSoup(driver.page_source, features="lxml")
+
+
+#/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-shelf-renderer/div[1]/div[2]/ytd-expanded-shelf-contents-renderer/div
+
+video_list = soup.find("html").find("body").find("ytd-app").find("div").find("ytd-page-manager").find("ytd-browse").find("ytd-two-column-browse-results-renderer").findAll("div")[1].find("ytd-section-list-renderer").findAll("div")[2].findAll("ytd-item-section-renderer")[1].findAll("div")[3].find("ytd-shelf-renderer").find("div")[1].find("div")[2].find("ytd-expanded-shelf-contents-renderer")
+
+
+driver.quit()
+
+print(video_list.prettify())
+
+
+
 response = requests.get(url)
 soup = BeautifulSoup(response.text, features="lxml")
 
-print(soup.nav)
+#/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+
+for content in soup.findAll("div", class_= "yt-lockup-content"):
+    try:
+        title = content.h3.a.text
+        print(title)
+
+        description = content.find('div', class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2").text
+        print(description)
+
+    except Exception as e:
+        description = None
+ """
+#print(soup.find("html").find("body").findAll("ytd-app"))
+
+#print(soup.find("html").find("body").find("ytd-app"))#.find("div").find("ytd-page-manager").find("ytd-browse").find("ytd-two-column-browse-results-renderer").findAll("div")[1].findAll("ytd-section-list-renderer").findAll("div")[2].findAll("ytd-item-section-renderer")[1])
