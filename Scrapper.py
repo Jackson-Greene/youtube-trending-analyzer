4a8429c29f039dfa6e0f256bff8912903be928af
Jackson Greene <19760458@student.curtin.edu.au>
2020-01-26 19:30:53 +0800

    comments, deleted test files, scrapper.py -> Scrapper.py

--- Scrapper.py
+++ Scrapper.py
@@ -4,16 +4,21 @@ import os
 from Video import Video
 
 
+#gets the current trending data, returns a list of Video (each containing title, desc, [tags])
 def getTrendingData():
 
     url = "https://www.youtube.com/feed/trending"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, features="lxml")
+
+    #list that will store Video objects
     videos_list = []
-    
-    print("hi")
-    for content in soup.findAll("div", class_= "yt-lockup-content"):
-        
+
+    #finds all the videos in the html
+    videos_in_html = soup.findAll("div", class_= "yt-lockup-content")
+
+    #for every video get title, desc and tags then store in a list
+    for content in videos_in_html:
         try:
             title = getTitle(content)
             description = getDesc(content)
@@ -28,25 +33,44 @@ def getTrendingData():
         except Exception as e:
             ...
 
-    print(str(videos_list[2]))
-
     return videos_list
+####
 
 
+####
 def getTitle(content):
+    
     return content.h3.a.text
+####
 
 
+####
 def getHref(content):
+    
     return content.h3.a.get("href")
+####
 
 
+####
 def getDesc(content):
+    
     return content.find('div', class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2").text
+####
 
 
+#gets tags of a video, need to have the video url to access html that has tags in it
 def getTags(video_url):
-    return ["test0", "test1"]
+    
+    tag_list = []
+    
+    response = requests.get(video_url)
+    soup = BeautifulSoup(response.text, features="lxml")
+    
+    tags_in_html = soup.findAll("meta", property="og:video:tag")
 
-
-getTrendingData()
+    #goes over every tag and adds them to a list
+    for tag in tags_in_html:
+        tag_list.append(tag.get("content"))
+    
+    return tag_list
+####
