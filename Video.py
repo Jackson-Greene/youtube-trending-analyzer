2685b48ef02136804e8b2eb50ce0ac5c589d5bf2
Jackson Greene <19760458@student.curtin.edu.au>
2020-01-26 18:10:51 +0800

    reformat of scrapper and video, removed href

--- Video.py
+++ Video.py
@@ -1,18 +1,12 @@
 class Video:
     title = ""
     desc = ""
-    hyperlink = ""
     tags = []
 	
-    def __init__(self, title, desc, hyperlink, tags):
+    def __init__(self, title, desc, tags):
 	    self._title = title
 	    self._desc = desc
-	    self._hyperlink = hyperlink
 	    self._tags = tags
-	
-    def creatVideo(title, desc, hyperlink, tags):
-	    video = Video(title, desc, hyperlink, tags)
-	    return video
 
     #Getters
     def getTitle(self):
@@ -20,9 +14,6 @@ class Video:
 
     def getDesc(self):
 	    return self._desc
-	
-    def getHyperlink(self):
-	    return self._hyperlink
 
     def getTags(self):
 	    return self._tags
@@ -33,16 +24,10 @@ class Video:
 	
     def setDesc(self, value):
 	    self._desc = value
-
-    def setHyperLink(self, value):
-	    self._hyperlink = value
 	
     def setTags(self, value):
 	    self.tags = value
 
     def __str__(self):
-	    return "Title: " + self._title + "\nDescription: " + self._desc + "\nHyperlink: " + self._hyperlink + "\nTags: " + self._tags
-
-#vid = Video("Banana", "Hello", "Apple", "Tomato")
-#print(vid)
+        return "title: {}\ndescription: {}\ntags: {}".format(str(self._title), str(self._desc), str(self._tags))
 
--- scrapper.py
+++ scrapper.py
@@ -6,33 +6,47 @@ from Video import Video
 
 def getTrendingData():
 
-  url = "https://www.youtube.com/feed/trending"
-
-  if os.path.exists("trending_videos_data.csv"):
-    os.remove("trending_videos_data.csv")
-  else:
-    ...
-
-  response = requests.get(url)
-
-  soup = BeautifulSoup(response.text, features="lxml")
-  videos_list = []
-  for content in soup.findAll("div", class_= "yt-lockup-content"):
-      
-      try:
-          title = content.h3.a.text
-
-          video_href = content.h3.a.get("href")
-
-          description = content.find('div', class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2").text
-
-          curr_video = Video(title, description, video_href, [1,2,3,4])
+    url = "https://www.youtube.com/feed/trending"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features="lxml")
+    videos_list = []
     
-          videos_list.append(curr_video)
+    print("hi")
+    for content in soup.findAll("div", class_= "yt-lockup-content"):
+        
+        try:
+            title = getTitle(content)
+            description = getDesc(content)
+            video_href = getHref(content)
+            video_url = "https://www.youtube.com{}".format(video_href)
+            tags = getTags(video_url)
 
-      except Exception as e:
-          description = None
+            curr_video = Video(title, description, tags)
+
+            videos_list.append(curr_video)
+
+        except Exception as e:
+            ...
+
+    print(str(videos_list[2]))
+
+    return videos_list
 
 
-  for video in videos_list: 
-    print(video.getTags())
+def getTitle(content):
+    return content.h3.a.text
+
+
+def getHref(content):
+    return content.h3.a.get("href")
+
+
+def getDesc(content):
+    return content.find('div', class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2").text
+
+
+def getTags(video_url):
+    return ["test0", "test1"]
+
+
+getTrendingData()
