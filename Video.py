a7c9664e7012fd27e99efdbe68239e1a3a06b0c9
Jackson Greene <19760458@student.curtin.edu.au>
2020-01-26 22:35:09 +0800

    spaces

--- Video.py
+++ Video.py
@@ -1,32 +1,41 @@
 class Video:
+
     title = ""
     desc = ""
     tags = []
 	
+
     def __init__(self, title, desc, tags):
 	    self._title = title
 	    self._desc = desc
 	    self._tags = tags
 
+
     #Getters
     def getTitle(self):
 	    return self._title
 
+
     def getDesc(self):
 	    return self._desc
+
 
     def getTags(self):
 	    return self._tags
 		
+
 	#Setters
     def setTitle(self, value):
 	    self._title = value
 	
+
     def setDesc(self, value):
 	    self._desc = value
 	
+
     def setTags(self, value):
 	    self.tags = value
+
 
     def __str__(self):
         return "title: {}\ndescription: {}\ntags: {}".format(str(self._title), str(self._desc), str(self._tags))
